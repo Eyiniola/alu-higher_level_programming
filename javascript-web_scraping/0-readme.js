d42b2d598ce648e30a7a48519151820729dd70e2
Eyiniola <e.fagbemi@alustudent.com>
2023-10-01 23:10:41 -0700

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const filePath = process.argv[2]; 
+const filePath = process.argv[2]; // Get the file path from command-line arguments
 
-
+// Check if a file path was provided
 if (!filePath) {
   console.error('Please provide a file path as the first argument.');
-  process.exit(1); 
+  process.exit(1); // Exit the script with an error code
 }
 
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('An error occurred while reading the file:', err);
-  } else {
-    console.log('File content:');
-    console.log(data);
-  }
-});
+try {
+  // Read the file synchronously in utf-8 encoding
+  const data = fs.readFileSync(filePath, 'utf-8');
+  console.log('File content:');
+  console.log(data);
+} catch (err) {
+  console.error('An error occurred while reading the file:', err);
+}
 
