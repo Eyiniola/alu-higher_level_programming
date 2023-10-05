ee53f8bb423f40a14e19c8b17ea1cdd00e90e251
Eyiniola <e.fagbemi@alustudent.com>
2023-10-05 09:41:44 -0700

    task 2

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,6 @@ const url = process.argv[2];
 request
 	.get(url)
 	.on('response' , function(response){
-		console.log('code: ${response.statusCode}');
+		console.log('code:' + response.statusCode);
 	}
 	);
