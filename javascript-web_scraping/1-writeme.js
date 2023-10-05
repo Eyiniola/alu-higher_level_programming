7fae60cceaacd34bff7a455a0b1ea5bed9a35903
Eyiniola <e.fagbemi@alustudent.com>
2023-10-05 07:31:51 -0700

    task 1

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const fs = require('fs');
+const FileNmae = process.argv[2];
+const content = process.argv[3];
+fs.writeFile(FileName, content, 'utf-8', function(err){
+	if (err){
+		console.log(err)});
