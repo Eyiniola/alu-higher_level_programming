971d2aa4f8cdfd9e396efb5c4a786fec8d6acd19
Eyiniola <e.fagbemi@alustudent.com>
2023-10-05 06:06:37 -0700

    task 0

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,22 +1,12 @@
 #!/usr/bin/node
 const fs = require('fs').promises;
 
-const filePath = process.argv[2]; // Get the file path from command-line arguments
-
-async function readFile() {
-  try {
-    const data = await fs.readFile(filePath, 'utf-8');
-    console.log('File content:');
-    console.log(data);
-  } catch (err) {
-    console.error('An error occurred while reading the file:', err);
-  }
-}
-
-if (!filePath) {
-  console.error('Please provide a file path as the first argument.');
-} else {
-  readFile();
-}
-
+const filePath = process.argv[2]; 
+const fs.readFileSync(filepath, "utf-8"
+function{
+	if(err){
+		console.log(err);}
+	else{
+		console.log(line);}
+});
 
