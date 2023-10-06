271ec1906793e7a297943760e5c764299832943b
Eyiniola <e.fagbemi@alustudent.com>
2023-10-06 06:26:44 -0700

    task 3

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -4,7 +4,7 @@ const request = require('request');
 const episodeId = process.argv[2];
 const apiUrl = `https://swapi-api.alx-tools.com/api/films/` + episodeId;
 
-request.get(apiUrl, function(error, response, body) => {
+request.get(apiUrl, function(error, response, body) {
 	if (error) {
 		console.log(error);
 	}
